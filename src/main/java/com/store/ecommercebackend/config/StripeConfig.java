f754bc9f80e08317ae514285dbee44f90e7d22ad
Noel <noelmugisha332@gmail.com>
2025-08-22 23:59:28 +0200

    Adding Stripe as paymentGateway

---
+++ src/main/java/com/store/ecommercebackend/config/StripeConfig.java
@@ -0,0 +1,16 @@
+package com.store.ecommercebackend.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+    @Value("${stripe.secret_key")
+    private String stripeSecretKey;
+
+    @PostConstruct
+    public void init () {
+
+    }
+}
